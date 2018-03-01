4292ddf809a32fa57dc009830f2660afc675b985
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2018-03-01 13:07:00 +0100

    ButterKnife y inicio de login

--- app/src/main/java/geogame/proyectoretos/UI/LoginActivity.java
+++ app/src/main/java/geogame/proyectoretos/UI/LoginActivity.java
@@ -1,23 +1,46 @@
 package geogame.proyectoretos.UI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import geogame.proyectoretos.R;
 
 public class LoginActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.et_email)
+    EditText email;
+    @BindView(R.id.et_pass)
+    EditText pass;
+    @BindViews({R.id.et_email,R.id.et_pass})
+    List<EditText> asd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
 
 
     }
 
+
+    @OnClick(R.id.btn_registrarse)
+    void registrarAdmin() {
+
+        Toast.makeText(getApplicationContext(), "asdasd", Toast.LENGTH_SHORT).show();
+
+
+    }
 
 
 }
--- app/src/main/java/geogame/proyectoretos/UI/MainActivity.java
+++ app/src/main/java/geogame/proyectoretos/UI/MainActivity.java
@@ -7,6 +7,7 @@ import android.arch.lifecycle.OnLifecycleEvent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,33 +16,36 @@ import geogame.proyectoretos.Chat.ChatActivity;
 import geogame.proyectoretos.R;
 import geogame.proyectoretos.UI.LoginActivity;
 
-public class MainActivity extends AppCompatActivity implements LifecycleOwner {
+public class MainActivity extends AppCompatActivity implements LifecycleObserver {
 
-
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        this.getLifecycle().addObserver(this);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    void comprobarLogin(){
+
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
+
+        if(currentUser==null){
+
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+
+        }
 
 
 
 
     }
 
-
-
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    public void comprobarUsuarioLogeado(){
-
-
-        startActivity(new Intent(getApplicationContext(), ChatActivity.class));
-
-
-
-    }
 
 }
