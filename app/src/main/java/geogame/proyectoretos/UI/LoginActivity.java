df94eae88c47babd3da3f6e09c4b1d5b5ccd8f3b
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2018-03-12 00:15:34 +0100

    mapa

--- app/src/main/java/geogame/proyectoretos/UI/LoginActivity.java
+++ app/src/main/java/geogame/proyectoretos/UI/LoginActivity.java
@@ -171,12 +171,6 @@ public class LoginActivity extends AppCompatActivity implements LifecycleObserve
         startActivity(i);
     }
 
-    @OnClick(R.id.btn_login_acercaDe)
-    void acercaDe(){
-
-        Intent i = new Intent(getApplicationContext(), AcercaDeActivity.class);
-        startActivity(i);
-    }
 
 
 }
