3f7945b803bd55d375b1922e606f182c717ec9d1
santiaf131 <santiago.alvarez.smr@gmail.com>
2018-03-13 19:33:32 +0100

    Arreglar vuelta

--- app/src/main/java/geogame/proyectoretos/UI/LocationModel.java
+++ app/src/main/java/geogame/proyectoretos/UI/LocationModel.java
@@ -4,6 +4,11 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.location.Location;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import geogame.proyectoretos.Data.entidades.Retos;
+
 /**
  * Created by Farra on 10/03/2018.
  */
@@ -11,8 +16,25 @@ import android.location.Location;
 public class LocationModel extends ViewModel{
 
     private MutableLiveData<Location> mLocation = new MutableLiveData<>();
-
+    private List<Retos> retos = new ArrayList<>();
+    boolean cargados=false;
     private int numReto=1;
+
+    public List<Retos> getRetos() {
+        return retos;
+    }
+
+    public void setRetos(List<Retos> retos) {
+        this.retos = retos;
+    }
+
+    public boolean isCargados() {
+        return cargados;
+    }
+
+    public void setCargados(boolean cargados) {
+        this.cargados = cargados;
+    }
 
     public int getNumReto() {
         return numReto;
