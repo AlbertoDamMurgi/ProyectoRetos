6a860866e581bdd3538f2899f21e6c0503d80b2b
santiaf131 <santiago.alvarez.smr@gmail.com>
2018-03-27 18:29:34 +0200

    Cargar partidas en lista

--- app/src/main/java/org/iesmurgi/reta2/UI/admin/ListarPartidasAdminActivity.java
+++ app/src/main/java/org/iesmurgi/reta2/UI/admin/ListarPartidasAdminActivity.java
@@ -4,9 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.iesmurgi.reta2.Chat.ChatAdapter;
+import org.iesmurgi.reta2.Data.entidades.Partidas;
 import org.iesmurgi.reta2.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -19,7 +32,7 @@ public class ListarPartidasAdminActivity extends AppCompatActivity {
     RecyclerView recyclerView;
 
 
-    private ArrayList<String> prueba = new ArrayList<>();
+    private ArrayList<String> partidas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +40,40 @@ public class ListarPartidasAdminActivity extends AppCompatActivity {
         setContentView(R.layout.activity_listar_partidas_admin);
         ButterKnife.bind(this);
 
-        prueba.add("partida1");
-        prueba.add("partida2");
-        prueba.add("partida3");
+        partidas.clear();
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        final String URL = "http://geogame.ml/api/obtener_partida_admin.php?idAdmin=1";
 
-        recyclerView.setAdapter(new ChatAdapter(prueba,getApplicationContext(),1));
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, URL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i=0;i<response.length();i++){
+                    try {
+                        JSONObject o = response.getJSONObject(i);
+                             partidas.add( o.getString("nombre"));
+
+                    } catch (JSONException e) {
+                        Log.e("Log Json error Partida", e.getMessage());
+                    }
+                }//endgfor
+
+                Log.e("LISTA Partida", response.toString());
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+                recyclerView.setAdapter(new ChatAdapter(partidas,getApplicationContext(),1));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Partida", error.getMessage());
+                Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(request);
+
+
 
     }
 }
