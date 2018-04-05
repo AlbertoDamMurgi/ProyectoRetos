bd64f51a531d42a520037f20b7e04a75fe7dcbe9
santiaf131 <santiago.alvarez.smr@gmail.com>
2018-04-05 20:42:49 +0200

    Up

--- app/src/main/java/org/iesmurgi/reta2/UI/admin/RegistroAdminActivity.java
+++ app/src/main/java/org/iesmurgi/reta2/UI/admin/RegistroAdminActivity.java
@@ -8,6 +8,13 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -19,6 +26,9 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.iesmurgi.reta2.R;
 import org.iesmurgi.reta2.UI.usuario.LoginModel;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistroAdminActivity extends AppCompatActivity {
 
@@ -75,9 +85,45 @@ public class RegistroAdminActivity extends AppCompatActivity {
                                             if (task.isSuccessful()) {
 
 
-                                                //TODO Registrar admin en db
+                                                //Registrar admin en db
 
 
+                                                final String URL = "http://geogame.ml/api/insertar_admin.php";
+                                                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                                                    @Override
+                                                    public void onResponse(String response) {
+                                                        Log.e("ON RESPONDE", response.toString());
+
+                                                        if (response.contains("success")) {
+                                                            finish();
+
+                                                        } else {
+
+                                                        }
+
+                                                    }
+                                                }, new Response.ErrorListener() {
+                                                    @Override
+                                                    public void onErrorResponse(VolleyError error) {
+                                                        Toast.makeText(getApplicationContext(), "Fallo del servidor", Toast.LENGTH_SHORT).show();
+                                                    }
+
+                                                }) {
+                                                    @Override
+                                                    protected Map<String, String> getParams() throws AuthFailureError {
+                                                        Map<String, String> params = new HashMap<>();
+                                                        params.put("username", correoadmin.getText().toString());
+                                                        params.put("passwd", "" + passadmin.getText().toString());
+
+                                                        return params;
+                                                    }
+                                                };
+
+                                                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                                                requestQueue.add(stringRequest);
+
+
+                                                // fin insertar a db
 
 
                                             } else {
