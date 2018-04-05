f9101df2906bb8e965a1e5599d0d6acb4a16f8b1
santiaf131 <santiago.alvarez.smr@gmail.com>
2018-04-06 01:41:53 +0200

    Comprobar permisos para jugar

--- app/src/main/java/org/iesmurgi/reta2/UI/usuario/RankingActivity.java
+++ app/src/main/java/org/iesmurgi/reta2/UI/usuario/RankingActivity.java
@@ -1,12 +1,26 @@
 package org.iesmurgi.reta2.UI.usuario;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.iesmurgi.reta2.Chat.ChatAdapter;
 import org.iesmurgi.reta2.Data.Objetos.RankingEquipos;
 import org.iesmurgi.reta2.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -14,10 +28,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RankingActivity extends AppCompatActivity {
-
+    int idPartida;
     @BindView(R.id.recycler_ranking)
     RecyclerView recyclerRanking;
 
+    ProgressDialog progressDialog;
     ArrayList<RankingEquipos> puntuacionEquipos = new ArrayList<>();
 
     @Override
@@ -25,32 +40,47 @@ public class RankingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         ButterKnife.bind(this);
+        progressDialog = new ProgressDialog(this);
+        idPartida=getIntent().getIntExtra("idPartida",0);
 
-
-        if (puntuacionEquipos.isEmpty()){
-            rellenarArray();
-        }
-
-        recyclerRanking.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-
-        recyclerRanking.setAdapter(new RankingAdapter(puntuacionEquipos, getApplicationContext()));
-
-
-
+        cargarDatos();
     }
 
 
-    public void rellenarArray(){
+    public void cargarDatos(){
+        progressDialog.setMessage("Cargando ranking...");
+        progressDialog.show();
+        final String URL = "http://geogame.ml/api/obtener_ranking_partida.php?idPartida="+idPartida;
 
-        puntuacionEquipos.add(new RankingEquipos("Los tiesos", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los tigres", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los pepes", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los strings", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los leones", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los saltys", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los farrators", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los ricos", 100));
-        puntuacionEquipos.add(new RankingEquipos("Los secos", 100));
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, URL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i=0;i<response.length();i++){
+                    try {
+                        JSONObject o = response.getJSONObject(i);
+                        puntuacionEquipos.add(new RankingEquipos(o.getString("username"),o.getInt("puntuaciontotal")));
+
+                    } catch (JSONException e) {
+                        Log.e("Log Json error Partida", e.getMessage());
+                    }
+                }//endgfor
+
+                Log.e("LISTA puntuacion", response.toString());
+
+                recyclerRanking.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                recyclerRanking.setAdapter(new RankingAdapter(puntuacionEquipos, getApplicationContext()));
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Partida", error.getMessage());
+                Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(request);
 
     }
 
