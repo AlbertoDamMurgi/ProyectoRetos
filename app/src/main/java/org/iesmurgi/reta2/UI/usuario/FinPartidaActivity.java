e1370ad9c6b14b513c0d00210cc84cd4a7fd369b
santiaf131 <santiago.alvarez.smr@gmail.com>
2018-04-06 03:22:08 +0200

    Botones nuevos y ranking

--- app/src/main/java/org/iesmurgi/reta2/UI/usuario/FinPartidaActivity.java
+++ app/src/main/java/org/iesmurgi/reta2/UI/usuario/FinPartidaActivity.java
@@ -5,20 +5,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.iesmurgi.reta2.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FinPartidaActivity extends AppCompatActivity {
+    int idPartida;
+    String nombreEquipo;
+    int puntosEquipo;
+
+
+    @BindView(R.id.txt_finpartida_nombreEquip)
+    TextView txt_finpartida_nombreEquip;
+
+    @BindView(R.id.txt_finpartida_puntos)
+    TextView txt_finpartida_puntos;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_partida);
 
+        ButterKnife.bind(this);
 
         Button btnVolver = findViewById(R.id.btn_finpartida_volverLogin);
         Button btnRanking = findViewById(R.id.btn_finpartida_verranking);
+
+        idPartida=getIntent().getIntExtra("idPartida",0);
+        nombreEquipo=getIntent().getStringExtra("nombreEquipo");
+        puntosEquipo=23;
+
+        txt_finpartida_nombreEquip.setText(nombreEquipo);
+        txt_finpartida_puntos.setText(""+puntosEquipo);
+
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +59,9 @@ public class FinPartidaActivity extends AppCompatActivity {
         btnRanking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Implementacion en versiones futuras...", Toast.LENGTH_SHORT).show();
+              startActivity(new Intent(getApplicationContext(),RankingActivity.class).putExtra("idPartida",idPartida));
             }
         });
+
     }
 }
