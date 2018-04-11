a25573c4ac11d4c5e0077d16f630c835b628f603
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2018-04-11 22:51:44 +0200

    asqweads

---
+++ app/src/main/java/org/iesmurgi/reta2/UI/admin/AdminPuntuarFotoDefinitiva.java
@@ -0,0 +1,15 @@
+package org.iesmurgi.reta2.UI.admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.iesmurgi.reta2.R;
+
+public class AdminPuntuarFotoDefinitiva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_puntuar_foto_definitiva);
+    }
+}
