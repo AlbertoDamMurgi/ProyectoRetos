702a02dbd1f0d4668d1b3ac1d4f93222da06f358
santiaf131 <santiago.alvarez.smr@gmail.com>
2018-04-13 00:35:58 +0200

    Meter punto a fotos

--- app/src/main/java/org/iesmurgi/reta2/UI/admin/AdminPuntuarFotoTransicion.java
+++ app/src/main/java/org/iesmurgi/reta2/UI/admin/AdminPuntuarFotoTransicion.java
@@ -1,5 +1,6 @@
 package org.iesmurgi.reta2.UI.admin;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
@@ -9,6 +10,13 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.database.DataSnapshot;
@@ -23,6 +31,8 @@ import org.iesmurgi.reta2.Chat.Chat;
 import org.iesmurgi.reta2.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,12 +41,14 @@ import butterknife.OnClick;
 public class AdminPuntuarFotoTransicion extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference myRef;
-    private String partida,usuario,nombrereto;
+    ProgressDialog progressDialog;
+    private String partida, usuario, nombrereto;
     private int idpartida;
     private ArrayList<String> urisString = new ArrayList<>();
     StorageReference storageReference;
     @BindView(R.id.imagen_descargada_admin)
     ImageView imagen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,22 +61,22 @@ public class AdminPuntuarFotoTransicion extends AppCompatActivity {
         storageReference = FirebaseStorage.getInstance().getReference();
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
+        progressDialog = new ProgressDialog(this);
 
 
         myRef.child("Imagenes").child(partida).child(usuario).child(nombrereto).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot data: dataSnapshot.getChildren()) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
                     Chat aux = data.getValue(Chat.class);
 
-                    urisString.add(aux.getMensaje().substring(aux.getMensaje().lastIndexOf("/")+1,aux.getMensaje().length()));
+                    urisString.add(aux.getMensaje().substring(aux.getMensaje().lastIndexOf("/") + 1, aux.getMensaje().length()));
 
                 }
                 StorageReference reference = storageReference.child("Imagenes").child(partida).child(usuario).child(nombrereto).child(urisString.get(0));
                 Glide.with(getApplicationContext()).using(new FirebaseImageLoader()).load(reference).into(imagen);
 
             }
-
 
 
             @Override
@@ -76,20 +88,55 @@ public class AdminPuntuarFotoTransicion extends AppCompatActivity {
     }
 
     @OnClick(R.id.btn_aceptarfoto_admin)
-    void aceptarFoto(){
+    void aceptarFoto() {
 
-        myRef.child("Imagenes").child(partida).child(usuario).child(nombrereto).removeValue();
-        Toast.makeText(this, "Foto validada, puntos añadidos con exito.", Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(getApplicationContext(),AdministrarPartidaAdminActivity.class).putExtra("ID",idpartida).putExtra("PARTIDA",partida));
+        progressDialog.setMessage("Aceptando foto ...");
+        progressDialog.show();
+
+        final String URL = "http://geogame.ml/api/update_puntuacion.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.contains("success")) {
+
+                    myRef.child("Imagenes").child(partida).child(usuario).child(nombrereto).removeValue();
+                    Toast.makeText(getApplicationContext(), "Foto validada, puntos añadidos con exito.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), AdministrarPartidaAdminActivity.class).putExtra("ID", idpartida).putExtra("PARTIDA", partida));
+                    finish();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Fallo al aceptar ", Toast.LENGTH_LONG).show();
+                }
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error al conectar con el servidor", Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("nombreUsuario", usuario);
+                params.put("nombreReto",nombrereto);
+                params.put("idPartida", ""+idpartida);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
 
     }
 
 
     @OnClick(R.id.btn_rechazarfoto_admin)
-    void rechazarFoto(){
+    void rechazarFoto() {
         myRef.child("Imagenes").child(partida).child(usuario).child(nombrereto).removeValue();
         Toast.makeText(this, "Foto rechazada, no se puntuará ese reto.", Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(getApplicationContext(),AdministrarPartidaAdminActivity.class).putExtra("ID",idpartida).putExtra("PARTIDA",partida));
+        startActivity(new Intent(getApplicationContext(), AdministrarPartidaAdminActivity.class).putExtra("ID", idpartida).putExtra("PARTIDA", partida));
 
     }
 
