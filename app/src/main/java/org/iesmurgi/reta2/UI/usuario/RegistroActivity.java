07dfa8ced64e5bf66006f14773d8017a8d0b105f
santiaf131 <santiago.alvarez.smr@gmail.com>
2018-04-23 17:30:22 +0200

    Rumanicos .SL

--- app/src/main/java/org/iesmurgi/reta2/UI/usuario/RegistroActivity.java
+++ app/src/main/java/org/iesmurgi/reta2/UI/usuario/RegistroActivity.java
@@ -19,11 +19,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthEmailException;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
 import org.iesmurgi.reta2.R;
 
 import java.util.HashMap;
@@ -33,10 +35,10 @@ public class RegistroActivity extends AppCompatActivity {
 
     private static final String TAG = "Registro_Equipo";
     @BindView(R.id.ed_nombre_equipo)
-     EditText nombreequipo;
+    EditText nombreequipo;
 
     @BindView(R.id.ed_pass_equipo)
-     EditText passequipo;
+    EditText passequipo;
 
     @BindView(R.id.ed_correo_equipo)
     EditText correoequipo;
@@ -60,94 +62,114 @@ public class RegistroActivity extends AppCompatActivity {
 
     }
 
+    private boolean comprobarRegistro() {
+        boolean ok = false;
+        if (
+                !nombreequipo.getText().toString().trim().isEmpty() && !correoequipo.getText().toString().trim().isEmpty() && !passequipo.getText().toString().trim().isEmpty() && !participantesEquipo.getText().toString().trim().isEmpty()
+                ) {
+            if (passequipo.getText().toString().trim().toCharArray().length >= 6) {
+                ok=true;
+            }else{
+                Toast.makeText(getApplicationContext(),"La contraseña debe tener 6 caracteres o mas ",Toast.LENGTH_LONG ).show();
+            }
+
+
+        }else{
+            Toast.makeText(getApplicationContext(),"Rellena todos los campos",Toast.LENGTH_LONG ).show();
+
+        }
+        return ok;
+    }
+
 
     @OnClick(R.id.btn_registro_equipo)
     void registrarEquipo() {
 
+        if (comprobarRegistro()) {
+            mAuth.createUserWithEmailAndPassword(correoequipo.getText().toString().trim(), passequipo.getText().toString().trim())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
 
-        mAuth.createUserWithEmailAndPassword(correoequipo.getText().toString().trim(), passequipo.getText().toString().trim())
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-
-                            mLoginModel.getConection().setValue(mAuth);
-                            mLoginModel.getUsuario().setValue(mAuth.getInstance().getCurrentUser());
-
-
-                            String name = nombreequipo.getText().toString().trim();
-                            UserProfileChangeRequest update = new UserProfileChangeRequest.Builder()
-                                    .setDisplayName(name)
-                                    .build();
-
-                            mLoginModel.getUsuario().getValue().updateProfile(update)
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                //Insertar a la base de datos
+                                mLoginModel.getConection().setValue(mAuth);
+                                mLoginModel.getUsuario().setValue(mAuth.getInstance().getCurrentUser());
 
 
+                                String name = nombreequipo.getText().toString().trim();
+                                UserProfileChangeRequest update = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(name)
+                                        .build();
+
+                                mLoginModel.getUsuario().getValue().updateProfile(update)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    //Insertar a la base de datos
 
 
-                                                final String URL = "http://geogame.ml/api/insertar_equipo.php";
-                                                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                                                    @Override
-                                                    public void onResponse(String response) {
-                                                        Log.e("ON RESPONDE", response.toString());
+                                                    final String URL = "http://geogame.ml/api/insertar_equipo.php";
+                                                    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                                                        @Override
+                                                        public void onResponse(String response) {
+                                                            Log.e("ON RESPONDE", response.toString());
 
-                                                        if (response.contains("success")) {
-                                                            finish();
+                                                            if (response.contains("success")) {
+                                                                Toast.makeText(RegistroActivity.this, "Registrado con exito ",
+                                                                        Toast.LENGTH_SHORT).show();
+                                                                finish();
 
-                                                        } else {
+                                                            } else {
+
+                                                            }
 
                                                         }
+                                                    }, new Response.ErrorListener() {
+                                                        @Override
+                                                        public void onErrorResponse(VolleyError error) {
+                                                            Toast.makeText(getApplicationContext(), "Fallo del servidor", Toast.LENGTH_SHORT).show();
+                                                        }
 
-                                                    }
-                                                }, new Response.ErrorListener() {
-                                                    @Override
-                                                    public void onErrorResponse(VolleyError error) {
-                                                        Toast.makeText(getApplicationContext(), "Fallo del servidor", Toast.LENGTH_SHORT).show();
-                                                    }
+                                                    }) {
+                                                        @Override
+                                                        protected Map<String, String> getParams() throws AuthFailureError {
+                                                            Map<String, String> params = new HashMap<>();
+                                                            params.put("username", nombreequipo.getText().toString().trim());
+                                                            params.put("correo", correoequipo.getText().toString().trim());
+                                                            params.put("passwd", "" + passequipo.getText().toString().trim());
+                                                            params.put("participantes", "" + participantesEquipo.getText().toString().trim());
+                                                            return params;
+                                                        }
+                                                    };
 
-                                                }) {
-                                                    @Override
-                                                    protected Map<String, String> getParams() throws AuthFailureError {
-                                                        Map<String, String> params = new HashMap<>();
-                                                        params.put("username", nombreequipo.getText().toString().trim());
-                                                        params.put("correo", correoequipo.getText().toString().trim());
-                                                        params.put("passwd", "" + passequipo.getText().toString().trim());
-                                                        params.put("participantes", ""+participantesEquipo.getText().toString().trim());
-                                                        return params;
-                                                    }
-                                                };
-
-                                                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-                                                requestQueue.add(stringRequest);
+                                                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                                                    requestQueue.add(stringRequest);
 
 
-                                                // fin insertar a db
+                                                    // fin insertar a db
 
 
-
-                                            } else {
-                                                Toast.makeText(RegistroActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Toast.makeText(RegistroActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                                }
                                             }
-                                        }
-                                    });
+                                        });
 
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(RegistroActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(RegistroActivity.this, "Correo no valido ",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
 
                         }
+                    });
 
-                    }
-                });
 
-    }
+        }
+    }//Onclick
 }
