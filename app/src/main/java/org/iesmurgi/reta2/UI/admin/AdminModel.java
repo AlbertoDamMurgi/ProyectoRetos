7b419201a2669c515e14e4a4df2c3e937473c737
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2018-06-10 22:51:41 +0200

    javadoc workermanager

--- app/src/main/java/org/iesmurgi/reta2/UI/admin/AdminModel.java
+++ app/src/main/java/org/iesmurgi/reta2/UI/admin/AdminModel.java
@@ -5,6 +5,15 @@ import android.arch.lifecycle.ViewModel;
 public class AdminModel extends ViewModel {
 
     private String codigoqr=null;
+    private int retoactual = 0;
+
+    public int getRetoactual() {
+        return retoactual;
+    }
+
+    public void setRetoactual(int retoactual) {
+        this.retoactual = retoactual;
+    }
 
     public String getCodigoqr() {
         return codigoqr;
